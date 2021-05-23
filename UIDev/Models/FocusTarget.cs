6f131220b7ef2d9370ac937f3abf81fd9a8e11aa
Alyssa Hollingshead <aly.hollingshead@gmail.com>
2021-05-23 04:13:33 -0500

    test tab closing

---
+++ UIDev/Models/FocusTarget.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIDev
+{
+    public class FocusTab
+    {
+        public Guid FocusTabId = new Guid();
+        public string Name = "";
+        public bool Open = true;
+        public List<FocusTarget> focusTargets = new List<FocusTarget>();
+
+        public FocusTab(string name, int id)
+        {
+            this.Name = name;
+            if (this.focusTargets.Any(t => t.Name == name))
+            {
+                this.focusTargets.Add(new FocusTarget()
+                {
+                    Id = id,
+                    Name = name
+                });
+            }
+        }
+
+        public List<string> getFocusTargetNames()
+        {
+            return focusTargets
+              .Select(t => t.Name)
+              .ToList();
+        }
+
+        public void addFocusTarget(string name, int id)
+        {
+            if (this.focusTargets.Any(t => t.Name == name))
+            {
+                this.focusTargets.Add(new FocusTarget()
+                {
+                    Id = id,
+                    Name = name
+                });
+            }
+        }
+    }
+
+    public class FocusTarget
+    {
+        public int Id;
+        public string Name;
+    }
+}
