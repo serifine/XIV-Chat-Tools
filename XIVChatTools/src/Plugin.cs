672d55913f6fdd19941cd834ef2a8d8ecc73dc6a
Alyssa Hollingshead <aly.hollingshead@gmail.com>
2024-08-16 12:52:37 -0500

    add world sheet reference

--- XIVChatTools/src/Plugin.cs
+++ XIVChatTools/src/Plugin.cs
@@ -18,6 +18,8 @@ using Dalamud.Plugin.Services;
 using Dalamud.Interface.Windowing;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using XIVChatTools.Services;
+using Lumina.Excel.GeneratedSheets;
+using Lumina.Excel;
 
 namespace XIVChatTools;
 
@@ -27,11 +29,13 @@ public class Plugin : IDalamudPlugin
 
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
     [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IClientState ClientState { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Logger { get; private set; } = null!;
     [PluginService] internal static ITargetManager TargetManager { get; private set; } = null!;
 
+    internal readonly ExcelSheet<World> WorldSheet;
     internal readonly PluginStateService PluginState;
     internal readonly MessageService MessageService;
     internal readonly WindowManagerService WindowManagerService;
@@ -61,6 +65,7 @@ public class Plugin : IDalamudPlugin
             PluginState = RegisterService<PluginStateService>();
             MessageService = RegisterService<MessageService>();
             WindowManagerService = RegisterService<WindowManagerService>();
+            WorldSheet = DataManager.GetExcelSheet<World>()!;
 
             ClientState.Login += OnLogin;
             ClientState.Logout += OnLogout;
