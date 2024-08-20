65a36417c75fe309a871693729b23ca06a52d3ea
Alyssa Hollingshead <aly.hollingshead@gmail.com>
2024-08-20 12:06:00 -0500

    add ObjectTable and re-order usings

--- XIVChatTools/src/Plugin.cs
+++ XIVChatTools/src/Plugin.cs
@@ -31,12 +31,13 @@ public class Plugin : IDalamudPlugin
 {
     public static string Name => "Chat Tools";
 
-    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
     [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IClientState ClientState { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Logger { get; private set; } = null!;
+    [PluginService] internal static IObjectTable ObjectTable { get; private set; } = null!;
+    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
     [PluginService] internal static ITargetManager TargetManager { get; private set; } = null!;
 
     internal readonly ExcelSheet<World> WorldSheet;
